e1d9f4912cf1e72e9a64f726ad97ff91afa37d06
Jose Rivera <nerivemu@gmail.com>
2021-02-11 03:16:38 +0000

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+import "../utils/style.css";
+// 2. Update the breakpoints as key-value pairs
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+});
+// 3. Extend the theme
+const theme = extendTheme({ breakpoints });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+export default MyApp;
