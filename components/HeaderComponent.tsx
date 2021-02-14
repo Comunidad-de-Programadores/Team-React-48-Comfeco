eab34933024a0b10dfd249dac7574cf3a40aabda
Jose Rivera <nerivemu@gmail.com>
2021-02-14 02:33:41 +0000

    cambios pequeños de stilos de header

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,35 +1,40 @@
-import React, { ReactElement } from 'react';
-import {Box, Image, Button } from '@chakra-ui/react';
+import React, { ReactElement } from "react";
+import { Box, Image, Button } from "@chakra-ui/react";
 
 interface Props {}
-    
+
 type MenuItemType = {
-    titulo: string;
-    link?: string;
+  titulo: string;
+  link?: string;
 };
 
 export default function HeaderComponent({}: Props): ReactElement {
+  const MenuItem = ({ titulo }: MenuItemType) => (
+    <Box ml="1em" mt="0.5em" fontSize="22px">
+      {titulo}
+    </Box>
+  );
 
-    const MenuItem = ({titulo}:MenuItemType) => (
-        <Box ml="1em" fontSize= "22px">{titulo}</Box>
-
-    )
-    
-    return (
-        <Box padding="1em" display="flex" justifyContent= "space-between" >
-            <Image src="/images/logo1.png" />
-            <Box display="flex" mt="4em" mr="-60em"  >
-                <MenuItem titulo="Home"   />
-                <MenuItem titulo="Sign in" />
-            </Box>
-             <Button mt="4em" mr="10em" bg="linear-gradient(90deg,#521e87 0.01%,#5c1d88 
-                14.55%,#751a8a 38.82%,#8a178c 54.92%,#8e1c87 56.27%,#c36e42 77.54%,#e4a017 92.6%,#f1b406 99.98%)" color='#fff'> 
-                  Registrase
-            </Button>
-            
+  return (
+    <Box padding="1em" px="2em" display="flex" justifyContent="space-between">
+      <Image src="/images/logo1.png" w="15%" mt="1em" />
+      <Box d="flex" mt="1em">
+        <Box display="flex">
+          <MenuItem titulo="Home" />
+          <MenuItem titulo="Sign in" />
         </Box>
-
-        
-    );
-
+        <Button
+          mr="4em"
+          p="1.5em"
+          borderRadius="1em"
+          ml="1em"
+          bg="linear-gradient(90deg,#521e87 0.01%,#5c1d88 
+            14.55%,#751a8a 38.82%,#8a178c 54.92%,#8e1c87 56.27%,#c36e42 77.54%,#e4a017 92.6%,#f1b406 99.98%)"
+          color="#fff"
+        >
+          Registrase
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from "react";
-import Link from "next/link";
 import Head from "next/head";
-import HeaderComponent from './HeaderComponent'
+import HeaderComponent from "./HeaderComponent";
 
 type Props = {
   children?: ReactNode;
