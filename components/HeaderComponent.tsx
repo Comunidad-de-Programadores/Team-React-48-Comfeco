e4fd7960f3db313200816a0f34287bb441801bf2
Jose Rivera <nerivemu@gmail.com>
2021-02-14 04:46:23 +0000

    link en header component. Close #14

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,35 +1,55 @@
 import React, { ReactElement } from "react";
 import { Box, Image, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Url } from "url";
 
 interface Props {}
 
 type MenuItemType = {
   titulo: string;
-  link?: string;
+  link?: Url | string;
 };
 
 export default function HeaderComponent({}: Props): ReactElement {
-  const MenuItem = ({ titulo }: MenuItemType) => (
-    <Box ml="1em" mt="0.5em" fontSize="22px">
+  const router = useRouter();
+  const MenuItem = ({ titulo, link }: MenuItemType) => (
+    <Box
+      cursor="pointer"
+      ml="1em"
+      mt="0.5em"
+      fontSize="22px"
+      onClick={() => link && router.push(link)}
+    >
       {titulo}
     </Box>
   );
 
   return (
     <Box padding="1em" px="2em" display="flex" justifyContent="space-between">
-      <Image src="/images/logo1.png" w="15%" mt="1em" />
+      <Image
+        src="/images/logo1.png"
+        w="15%"
+        mt="1em"
+        onClick={() => router.push("/")}
+      />
       <Box d="flex" mt="1em">
         <Box display="flex">
-          <MenuItem titulo="Home" />
-          <MenuItem titulo="Sign in" />
+          <MenuItem titulo="Home" link="/" />
+          <MenuItem titulo="Sign in" link="/sign-in" />
         </Box>
         <Button
           mr="4em"
           p="1.5em"
           borderRadius="1em"
+          onClick={() => router.push("/sign-up")}
           ml="1em"
-          bg="linear-gradient(90deg,#521e87 0.01%,#5c1d88 
-            14.55%,#751a8a 38.82%,#8a178c 54.92%,#8e1c87 56.27%,#c36e42 77.54%,#e4a017 92.6%,#f1b406 99.98%)"
+          bg="linear-gradient(90deg,rgba(82, 30, 135, 0.8) 0.01%,rgba(91, 29, 136, 0.8) 
+            14.55%,rgba(117, 26, 138, 0.8) 38.82%,rgba(138, 23, 140, 0.8) 54.92%,rgba(142, 28, 134, 0.8) 56.27%,rgba(195, 109, 66, 0.8) 77.54%,rgba(228, 160, 23, 0.8) 92.6%,rgba(241, 178, 6, 0.8) 99.98%)"
+          _hover={{
+            background:
+              "linear-gradient(90deg,rgba(82, 30, 135, 0.8) 0.01%,rgba(91, 29, 136, 0.8)14.55%,rgba(117, 26, 138, 0.8) 38.82%,rgba(138, 23, 140, 0.8) 54.92%,rgba(142, 28, 134, 0.8) 56.27%,rgba(195, 109, 66, 0.8) 77.54%,rgba(228, 160, 23, 0.8) 92.6%,rgba(241, 178, 6, 0.8) 99.98%)",
+            color: "#fafafa",
+          }}
           color="#fff"
         >
           Registrase
