a1201321faab225212ac6c12cd4522918f340769
aybike <tonn.raus09@gmail.com>
2021-02-16 13:39:08 +0100

    feat(Footer): Layout established

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,32 +1,34 @@
 import React, { ReactElement } from 'react';
-import { Box, Image, Button } from "@chakra-ui/react";
+import { Box, Text, Grid, GridItem } from "@chakra-ui/react";
 import {useRouter} from "next/router";
 import {Url} from "url";
 
 export default function FooterComponent(): ReactElement {
     const router = useRouter();
     return (
-       <Box>
-           <Box>
-            <a href="#">
-                <img src="" alt="logo-conf"/>
-            </a>
-            <Box>
+       <Box w="100%" h="4rem">
+           <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+            <GridItem w="100%" h="100%" >
+                <a  href="#">
+                    <img src="" alt="logo-conf"/>
+                </a>
+            </GridItem>
+            <GridItem w="100%" h="100%" colSpan={2} >
                 <a href="#">Terminos y Condiciones</a>
                 <a href="#">Proteccion de Datos</a>
                 <a href="#">Politica de Privacidad</a>
-            </Box>
-            <Box>
+            </GridItem>
+            <GridItem w="100%" h="100%">
                 <p>Siguenos en nuestras Redes:</p>
-                <Box>
+                <Box w="100%" >
                     <a href="#">Github</a>
                     <a href="#">Youtube</a>
                     <a href="#">Github</a>
                     <a href="#">Discord</a>
                 </Box>
-            </Box>
+            </GridItem>
+           </Grid>
             <p>©2021 Comfeco</p>
-           </Box>
        </Box>
     )
 }
