fdf87e4cfdddc338216131a5354e890dde725f03
aybike <tonn.raus09@gmail.com>
2021-02-16 12:46:19 +0100

    feat: footer component created

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+export default function FooterComponent(): ReactElement {
+    return (
+        <div>
+            <p>Hola mundo</p>
+        </div>
+    )
+}
+
+
