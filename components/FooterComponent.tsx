63962901a20dc950367cffa6d6ed254d12aa90cd
aybike <tonn.raus09@gmail.com>
2021-02-16 14:30:45 +0100

    test(Footer): Footer under revision

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Box, Text, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Image, Text, Grid, GridItem, Link } from "@chakra-ui/react";
 import {useRouter} from "next/router";
 import {Url} from "url";
 
@@ -7,28 +7,28 @@ export default function FooterComponent(): ReactElement {
     const router = useRouter();
     return (
        <Box w="100%" h="4rem">
-           <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-            <GridItem w="100%" h="100%" >
-                <a  href="#">
-                    <img src="/images/logo1.png" alt="logo-conf"/>
-                </a>
+           <Grid templateColumns="repeat(4, 1fr)" gap={6} p="0 3rem">
+            <GridItem w="100%" h="100%" bg="blue">
+                <Link  href="#">
+                    <Image w="50%" src="/images/logo1.png" alt="logo-conf"/>
+                </Link>
             </GridItem>
-            <GridItem w="100%" h="100%" colSpan={2} >
-                <a href="#">Terminos y Condiciones</a>
-                <a href="#">Proteccion de Datos</a>
-                <a href="#">Politica de Privacidad</a>
+            <GridItem w="100%" h="50%" colSpan={2} fontSize="14px" textAlign="center" bg="brown">
+                <Link  color="#D740DA" href="#">Terminos y Condiciones</Link>
+                <Link  color="#D740DA" href="#" mr="1rem" ml="1rem">Proteccion de Datos</Link>
+                <Link  color="#D740DA" href="#">Politica de Privacidad</Link>
             </GridItem>
-            <GridItem w="100%" h="100%">
-                <p>Siguenos en nuestras Redes:</p>
-                <Box w="100%" >
-                    <a href="#"><img src="/images/github-small.png" alt="Github Logo"/></a>
-                    <a href="#"><img src="/images/youtube-small.png" alt="Youtube Logo"/></a>
-                    <a href="#"><img src="/images/facebook-small.png" alt="Facebook Logo"/></a>
-                    <a href="#"><img src="/images/discord-small.png" alt="Discord Logo"/></a>
+            <GridItem w="100%" h="100%" bg="yellow">
+                <Text>Siguenos en nuestras Redes:</Text>
+                <Box w="100%" display="flex" justifyContent="space-evenly" >
+                    <Link href="#"><Image boxSize="40px" src="/images/github-small.png" alt="Github Logo"/></Link>
+                    <Link href="#"><Image boxSize="40px" src="/images/youtube-small.png" alt="Youtube Logo"/></Link>
+                    <Link href="#"><Image boxSize="40px" src="/images/facebook-small.png" alt="Facebook Logo"/></Link>
+                    <Link href="#"><Image boxSize="40px" src="/images/discord-small.png" alt="Discord Logo"/></Link>
                 </Box>
             </GridItem>
            </Grid>
-            <p>©2021 Comfeco</p>
+            <Text fontSize="10px" textAlign="center">©2021 Comfeco</Text>
        </Box>
     )
 }
