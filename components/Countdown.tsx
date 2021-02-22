3352b7704f51a6cfc2ed09cd03ca1b48890a180b
aybike <tonn.raus09@gmail.com>
2021-02-22 12:33:47 +0100

    refactor(countdown): presentate component to revision

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -6,33 +6,32 @@ export default function Countdown() {
         <section style={{
             width: "100%",
             height: "30rem",
-            backgroundColor: "burlywood",
             display: "flex",
             justifyContent:"center",
             alignItems: "center",
         }}>
-        <Box bg="gray" w="80%" h="80%">
-            <Heading color="#9A30A6" as="h4" size="lg" textAlign="center" mb="2rem">Preparate lo bueno esta por venir</Heading>
-                <Box w="100%" h="15rem" display="flex" justifyContent="center" bg="blue">
-                    <Box display="flex" bg="brown" justifyContent="space-evenly" w="80%">
-                            <Box position="relative" w="20%" bg="palegoldenrod">
-                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center" bg="red" h="70%">365</Text>
-                                <Text fontSize="30px" bg="pink" h="30%" textAlign="center">Dias</Text>
+        <Box  w="80%" h="80%">
+            <Heading color="#272727" as="h4" size="lg" textAlign="center" mb="2rem">Preparate lo bueno esta por venir</Heading>
+                <Box w="100%" h="15rem" display="flex" justifyContent="center">
+                    <Box display="flex"  justifyContent="space-evenly" w="80%">
+                            <Box position="relative" w="20%" >
+                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center"  h="70%">365</Text>
+                                <Text fontSize="30px"  h="30%" textAlign="center">Dias</Text>
                                 <span style={{position: "absolute", bottom: "40%", left: "100%", fontSize: "80px", fontWeight: "bold"}}>:</span>
                             </Box>
-                            <Box position="relative" w="20%" bg="palegoldenrod">
-                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center" bg="red" h="70%">24</Text>
-                                <Text fontSize="30px" bg="pink" h="30%" textAlign="center">Horas</Text>
+                            <Box position="relative" w="20%" >
+                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center"  h="70%">24</Text>
+                                <Text fontSize="30px" h="30%" textAlign="center">Horas</Text>
                                 <span style={{position: "absolute", bottom: "40%", left: "100%", fontSize: "80px", fontWeight: "bold"}}>:</span>
                             </Box>
-                            <Box position="relative" w="20%" bg="palegoldenrod">  
-                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center" bg="red" h="70%">60</Text>
-                                <Text fontSize="30px" bg="pink" h="30%" textAlign="center">Minutos</Text>
+                            <Box position="relative" w="20%" >  
+                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center"  h="70%">60</Text>
+                                <Text fontSize="30px"  h="30%" textAlign="center">Minutos</Text>
                                 <span style={{position: "absolute", bottom: "40%", left: "100%", fontSize: "80px", fontWeight: "bold"}}>:</span>
                             </Box>
-                            <Box position="relative" w="20%" bg="palegoldenrod">
-                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center" bg="red" h="70%">60</Text>
-                                <Text fontSize="30px" bg="pink" h="30%" textAlign="center">Segundos</Text>
+                            <Box position="relative" w="20%" >
+                                <Text fontSize="5rem" fontWeight="bold" display="flex" alignItems="center" justifyContent="center"  h="70%">60</Text>
+                                <Text fontSize="30px"  h="30%" textAlign="center">Segundos</Text>
                             </Box>
                         </Box>
 
