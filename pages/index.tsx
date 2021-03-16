0ed9bd450a21c36343d1b79672e8eeb57ce1f240
aybike <tonn.raus09@gmail.com>
2021-03-16 20:41:23 +0100

    refactor(pages): asigning head titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import { useSession } from "next-auth/client";
 import Landing from "../components/landing";
 import Landing2 from "../components/landing/Landing2";
 
+<<<<<<< HEAD
 export default function index(): JSX.Element {
   const [session, loading] = useSession();
+=======
+const IndexPage = () => (
+  <Layout title="Home">
+    <Landing />
+  </Layout>
+);
+>>>>>>> 1ec4541 (refactor(pages): asigning head titles)
 
   return (
     <>
