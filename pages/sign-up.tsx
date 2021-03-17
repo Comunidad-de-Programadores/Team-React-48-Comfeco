627d03cb83a26d26573db3fe45d8ed05f33000b3
DiangoGav <diangogavidia@gmail.com>
2021-03-16 23:47:41 -0400

    fix: signup url endpoint

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -40,7 +40,7 @@ export default function SignUp({}: Props): ReactElement {
 
   const register = async () => {
     try {
-      const response = await fetch("http://localhost:3000/api/users", {
+      const response = await fetch("/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
