4f604ee83f0b79f10d5716259f34d49e36b05a2b
Nekitory <nerivemu@gmail.com>
2021-03-17 21:25:20 +0000

    commit to push 3.0

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -77,7 +77,7 @@ const providers = [
 ];
 
 const callbacks = {
-  async jwt(token, user, account, profile, isNewUser) {
+  async jwt(token: any, user: any, account: any, profile: any, isNewUser: any) {
     // if (user) {
     //   token.accessToken = user.data.token;
     // }
@@ -85,7 +85,7 @@ const callbacks = {
     return token;
   },
 
-  async session(session, token) {
+  async session(session: any, token: any) {
     session.accessToken = token.accessToken;
     return session;
   },
