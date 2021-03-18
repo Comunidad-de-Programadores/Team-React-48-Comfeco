d85fee3bd28068bbb05dcebaf28a61bd129a79d6
aybike <tonn.raus09@gmail.com>
2021-03-18 13:38:05 +0100

    feat(envlocal): add port variable

--- utils/validators/loginValidator.ts
+++ utils/validators/loginValidator.ts
@@ -7,21 +7,13 @@ export default function loginValidator(values: Login): LoginErrors {
   };
 
   if (!values.email) {
-<<<<<<< HEAD
     errors.email = "Email es requerido";
-=======
-    errors.email = "Email is required";
->>>>>>> 226795e (feat(notfound): add page for not found route)
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
     errors.email = "Email no válido";
   }
 
   if (!values.password) {
-<<<<<<< HEAD
     errors.password = "Contraseña es requerida";
-=======
-    errors.password = "Password is required";
->>>>>>> 226795e (feat(notfound): add page for not found route)
   } else if (values.password.length < 6) {
     errors.password = "La contraseña debe tener como minimo 6 carácteres";
   }
