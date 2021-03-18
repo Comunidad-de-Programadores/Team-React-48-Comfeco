e6afe0fb7aa55a70fe69242db3c7a0e753f14bcc
aybike <tonn.raus09@gmail.com>
2021-03-18 10:51:05 +0100

    refactor(headerlogin): header fixed

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -14,6 +14,30 @@ export default function SignUp(): JSX.Element {
   }, []);
 
   const color = "#5555555";
+  const bg = `linear-gradient(90deg,
+    rgba(82, 30, 135, 0.8) 0.01%,
+    rgba(91, 29, 136, 0.8) 14.55%,
+    rgba(117, 26, 138, 0.8) 38.82%,
+    rgba(138, 23, 140, 0.8) 54.92%,
+    rgba(142, 28, 134, 0.8) 56.27%,
+    rgba(195, 109, 66, 0.8) 77.54%,
+    rgba(228, 160, 23, 0.8) 92.6%,
+    rgba(241, 178, 6, 0.8) 99.98%)`;
+  const hover = {
+    background:
+      "linear-gradient(90deg,rgba(82, 30, 135, 0.8) 0.01%,rgba(91, 29, 136, 0.8)14.55%,rgba(117, 26, 138, 0.8) 38.82%,rgba(138, 23, 140, 0.8) 54.92%,rgba(142, 28, 134, 0.8) 56.27%,rgba(195, 109, 66, 0.8) 77.54%,rgba(228, 160, 23, 0.8) 92.6%,rgba(241, 178, 6, 0.8) 99.98%)",
+    color: "#fafafa",
+  };
+  const boxStyle = {
+    Position: "absolute",
+    top: "50%",
+    zIndex: 1,
+    borderTopWidth: "2px",
+    borderColor: "#E1E2E7",
+    width: "100%",
+  };
+  const bgText = "#F3F2F3";
+  const colorText = "#85898D";
 
   return (
     <Layout title="Registro">
