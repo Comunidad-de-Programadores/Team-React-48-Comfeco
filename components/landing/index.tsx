ca5647fdef1263297e12a49d2b905fdf33ff7116
Jose Rivera <nerivemu@gmail.com>
2021-03-19 12:18:33 +0000

    layout of landing login

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -3,69 +3,72 @@ import React, { ReactElement } from "react";
 import Countdown from "./Countdown";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
+import Layout from "../Layout";
 interface Props {}
 
 export default function LandingUser({}: Props): ReactElement {
   return (
-    <Grid
-      templateColumns={{
-        sm: "1fr",
-        md: "repeat(9, 1fr)",
-      }}
-      rowGap={{
-        sm: "3rem",
-        md: "initial",
-      }}
-      margin={{ sm: "0 0 2rem", md: "initial" }}
-    >
-      <GridItem
-        colSpan={{
-          sm: 1,
-          md: 2,
+    <Layout title="Home">
+      <Grid
+        templateColumns={{
+          sm: "1fr",
+          md: "repeat(9, 1fr)",
         }}
-        rowStart={{
-          sm: 2,
-          md: 1,
-        }}
-        rowEnd={{
-          sm: 3,
-          md: 1,
-        }}
-        margin={{
-          sm: "0 0 3rem",
+        rowGap={{
+          sm: "3rem",
           md: "initial",
         }}
+        margin={{ sm: "0 0 2rem", md: "initial" }}
       >
-        <LeftSidebar />
-      </GridItem>
-      <GridItem
-        colSpan={{
-          sm: 1,
-          md: 5,
-        }}
-        rowStart={{
-          sm: 1,
-          md: 1,
-        }}
-        rowEnd={{
-          sm: 2,
-          md: 1,
-        }}
-        padding={{
-          sm: "0 1rem",
-          md: "initial",
-        }}
-      >
-        <Countdown />
-      </GridItem>
-      <GridItem
-        colSpan={{
-          sm: 1,
-          md: 2,
-        }}
-      >
-        <RightSidebar />
-      </GridItem>
-    </Grid>
+        <GridItem
+          colSpan={{
+            sm: 1,
+            md: 2,
+          }}
+          rowStart={{
+            sm: 2,
+            md: 1,
+          }}
+          rowEnd={{
+            sm: 3,
+            md: 1,
+          }}
+          margin={{
+            sm: "0 0 3rem",
+            md: "initial",
+          }}
+        >
+          <LeftSidebar />
+        </GridItem>
+        <GridItem
+          colSpan={{
+            sm: 1,
+            md: 5,
+          }}
+          rowStart={{
+            sm: 1,
+            md: 1,
+          }}
+          rowEnd={{
+            sm: 2,
+            md: 1,
+          }}
+          padding={{
+            sm: "0 1rem",
+            md: "initial",
+          }}
+        >
+          <Countdown />
+        </GridItem>
+        <GridItem
+          colSpan={{
+            sm: 1,
+            md: 2,
+          }}
+        >
+          <RightSidebar />
+        </GridItem>
+      </Grid>
+    </Layout>
   );
 }
