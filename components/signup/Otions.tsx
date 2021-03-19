4a20346d9f3b0ba0732dc42d00d35d512aea75bf
aybike <tonn.raus09@gmail.com>
2021-03-19 18:15:46 +0100

    style(signup): add styles for signup

--- components/signup/Otions.tsx
+++ components/signup/Otions.tsx
@@ -11,17 +11,16 @@ function Otions(): JSX.Element {
     borderColor: "#E1E2E7",
     width: "100%",
   };
-  const colorText = "white";
+  const colorText = "#515151";
   return (
     <Box w={{ sm: "100%", lg: "50%" }}>
       <Box w="100%" d="flex" flexDir="column" alignItems="center">
-        <Divider w={{ sm: "80%" }} my={{ sm: "10px" }} />
+        <Divider w={{ sm: "80%" }} my={{ sm: "10px", lg: "10px" }} />
         <Text
           w={{ sm: "10rem" }}
           textAlign="center"
           justifySelf="center"
           fontSize={{ sm: "14px", md: "14px", lg: "16px" }}
-          padding={{ lg: "1rem" }}
           color={colorText}
         >
           O continua con:
@@ -32,23 +31,26 @@ function Otions(): JSX.Element {
         display="flex"
         justifyContent="space-evenly"
         alignItems="center"
-        mt={{ lg: "2rem" }}
       >
         <Box
-          w={{ sm: "50px", lg: "100px" }}
-          h={{ sm: "50px", lg: "100px" }}
+          cursor="pointer"
+          w={{ sm: "50px", lg: "80px" }}
+          h={{ sm: "50px", lg: "80px" }}
           bg="white"
-          borderRadius={{ sm: "5px", lg: "1.5rem" }}
+          borderRadius={{ sm: "5px", lg: "7px" }}
           padding={{ sm: "10px", lg: "1.5rem" }}
+          filter="drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
         >
           <GrGoogle size="small" />
         </Box>
         <Box
-          w={{ sm: "50px", lg: "100px" }}
-          h={{ sm: "50px", lg: "100px" }}
+          cursor="pointer"
+          w={{ sm: "50px", lg: "80px" }}
+          h={{ sm: "50px", lg: "80px" }}
           bg="white"
-          borderRadius={{ sm: "5px", lg: "1.5rem" }}
+          borderRadius={{ sm: "5px", lg: "7px" }}
           padding={{ sm: "10px", lg: "1.5rem" }}
+          filter="drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))"
         >
           <GrFacebook size="small" />
         </Box>
